f083515e575715bf618cc50292b0e8a014dbc803
uropek97 <chebota2@gmail.com>
2023-03-03 01:39:35 +0200

    Готовая пирамидальная сортировка

--- lesson-2/lesson-2/Program.cs
+++ lesson-2/lesson-2/Program.cs
@@ -4,7 +4,70 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int[] arr = createRdmArray(30);
+            Console.WriteLine("До: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}  ");
+            }
+            Console.WriteLine();
+            HeapSort(arr);
+            Console.WriteLine("После: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]}  ");
+            }
+        }
+
+        public static int[] createRdmArray(int length)
+        {
+            Random rdm = new Random();
+            int[] arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                arr[i] = rdm.Next(100);
+            }
+            return arr;
+        }
+
+        static void Heapify(int[] arr, int n, int i)
+        {
+            int largest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            if (largest != i)
+            {
+                int temp = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = temp;
+
+                Heapify(arr, n, largest);
+            }
+        }
+
+        // Основная функция, выполняющая пирамидальную сортировку
+        static void HeapSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(arr, n, i);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                Heapify(arr, i, 0);
+            }
         }
     }
 }
